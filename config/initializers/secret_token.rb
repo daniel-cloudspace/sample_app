5393e5edaab259987f698d17e9bbe82ed5ac009d
hudson <hudson@hudson.cfl.rr.com>
2011-02-04 21:06:08 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '939ccff275eda4e1bc630339300e5b6db468f2012baab93b4ef0e23855db433d219b35e3392e4d29ec769d84ae259d010142e1a642b99cb6284361d1fc698aa2'
